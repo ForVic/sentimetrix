0e3f94c57df76469524a0cbe8a1b2a93ebc33520
nathan-marquez <nquezt@gmail.com>
2023-10-21 22:50:27 -0400

    [Frontend] [Interviewer Client] [ChooseInterviewType] init styles

--- frontend/sentimetrix-client/src/InterviewerComponents/InterviewChoiceComponent.js
+++ frontend/sentimetrix-client/src/InterviewerComponents/InterviewChoiceComponent.js
@@ -1,25 +1,48 @@
 // src/InterviewerComponents/InterviewChoiceComponent.js
 
 import React, { useState } from "react";
+import "./styles/InterviewChoiceComponent.css"; // Import the CSS file
+import { Link, useNavigate } from "react-router-dom";
 
-function InterviewChoiceComponent({ handleTypeSelection }) {
-  const [selectedType, setSelectedType] = useState("");
+function InterviewChoiceComponent() {
+  const navigate = useNavigate();
 
-  const handleLoclaTypeSelection = (type) => {
-    setSelectedType(type);
-    handleTypeSelection(type);
+  const handleLocalTypeSelection = (selectedType) => {
+    navigate("/create-interview/" + selectedType);
   };
 
   return (
     <div>
-      <h2>Select Interview Type:</h2>
-      <button onClick={() => handleLoclaTypeSelection("video-screening")}>
-        Video Screening
-      </button>
-      <button onClick={() => handleLoclaTypeSelection("task-analysis")}>
-        Task Analysis
-      </button>
-      {selectedType && <p>Selected: {selectedType}</p>}
+      <h2 className={"title"}>Select type of study to set up:</h2>
+      <div className="interview-choice-container ">
+        {/* Video Screening Card */}
+        <div className="interview-card">
+          <h3>Video Screening</h3>
+          <p>Conduct video-based interviews with candidates.</p>
+          <div className="platforms-row">
+            <div className="platform-box">Platform 1</div>
+            <div className="platform-box">Platform 2</div>
+            <div className="platform-box">Platform 3</div>
+          </div>
+          <button onClick={() => handleLocalTypeSelection("video-screening")}>
+            Select
+          </button>
+        </div>
+
+        {/* Task Analysis Card */}
+        <div className="interview-card">
+          <h3>Task Analysis</h3>
+          <p>Conduct task-oriented interviews with candidates.</p>
+          <div className="platforms-row">
+            <div className="platform-box">Platform 1</div>
+            <div className="platform-box">Platform 2</div>
+            <div className="platform-box">Platform 3</div>
+          </div>
+          <button onClick={() => handleLocalTypeSelection("task-analysis")}>
+            Select
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/sentimetrix-client/src/InterviewerViews/ChooseInterviewTypeView.js
+++ frontend/sentimetrix-client/src/InterviewerViews/ChooseInterviewTypeView.js
@@ -2,30 +2,20 @@
 
 import React, { useState } from "react";
 import InterviewChoiceComponent from "../InterviewerComponents/InterviewChoiceComponent";
-import { Link, useNavigate } from "react-router-dom";
+import "./styles/ChooseInterviewTypeView.css";
+import sentimetrixLogo from "../imgs/sentimetrix_logo.png";
 
 function ChooseInterviewTypeView() {
-  const navigate = useNavigate();
-
-  const [selectedType, setSelectedType] = useState("");
-
-  const handleTypeSelection = (type) => {
-    setSelectedType(type);
-  };
-
-  const handleNextClick = () => {
-    if (selectedType) {
-      navigate("/create-interview/" + selectedType);
-    } else {
-      // Handle the case where no type is selected
-    }
-  };
-
   return (
     <div>
-      <h1>Choose Interview Type</h1>
-      <InterviewChoiceComponent handleTypeSelection={handleTypeSelection} />
-      <button onClick={handleNextClick}>Next</button>
+      <img
+        className="sentimetrix-logo"
+        src={sentimetrixLogo}
+        alt="Sentimetrix"
+      />
+      <div className="section-wrapper">
+        <InterviewChoiceComponent />
+      </div>
     </div>
   );
 }
